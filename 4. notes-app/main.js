0462441c09a8050d589ee09d0958c036f76fe907
overaction <52850392+overaction@users.noreply.github.com>
2020-10-12 15:47:27 +0900

    4. notes app - save to localstorage

--- 4. notes-app/main.js
+++ 4. notes-app/main.js
@@ -1,7 +1,15 @@
 'use strict';
 const addBtn = document.querySelector('.add');
 
-function addNewNote() {
+const notes = JSON.parse(localStorage.getItem('notes'));
+
+if(notes) {
+    notes.forEach((note) => {
+        addNewNote(note);
+    })
+}
+
+function addNewNote(text = "") {
     const note = document.createElement('div');
     note.classList.add('note');
 
@@ -11,9 +19,7 @@ function addNewNote() {
         <button class="edit"><i class="far fa-edit"></i></button>
         <button class="delete"><i class="fas fa-trash-alt"></i></button>
     </div>
-    <div class="main hidden">
-        
-    </div>
+    <div class="main hidden"></div>
     <textarea></textarea>
     </section>
     `;
@@ -25,6 +31,8 @@ function addNewNote() {
 
     const main = note.querySelector('.main');
     const textArea = note.querySelector('textarea');
+    textArea.value = text;
+    main.innerHTML = marked(text);
 
     editBtn.addEventListener('click', () => {
         main.classList.toggle('hidden');
@@ -33,15 +41,28 @@ function addNewNote() {
 
     deleteBtn.addEventListener('click', () => {
         note.remove();
+        updateLS();
     })
 
     textArea.addEventListener('input', (e) => {
         const value = e.target.value;
 
         main.innerHTML = marked(value);
+        updateLS();
     })
 }
 
 addBtn.addEventListener('click', () => {
     addNewNote();
-})
+    updateLS();
+});
+
+function updateLS() {
+    const notesText = document.querySelectorAll('textarea');
+    const notes = [];
+
+    notesText.forEach((note) => {
+        notes.push(note.value);
+    })
+    localStorage.setItem('notes',JSON.stringify(notes));
+}
