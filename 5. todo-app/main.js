6bf38a0812db891210f83704253ceb8fa6705f95
overaction <52850392+overaction@users.noreply.github.com>
2020-10-13 15:48:19 +0900

    5. todo app - submit todo
    submit when you press 'enter'

--- 5. todo-app/main.js
+++ 5. todo-app/main.js
@@ -1,1 +1,23 @@
 'use strict';
+const input = document.getElementById('input');
+const todos = document.getElementById('todos');
+
+function pressEnter() {
+    if(event.keyCode === 13) {
+        addTodo();
+    }
+}
+
+function addTodo() {
+    const todo = document.createElement('div');
+    todo.classList.add('todo');
+    todo.innerHTML = `
+    <div class="todo-left">
+        <button class="clear"><i class="far fa-circle"></i></button>
+        <div class="todo-text">${input.value}</div>
+    </div>
+    <button class="delete"><i class="far fa-window-close"></i></button>
+    `;
+    todos.appendChild(todo);
+    input.value = '';
+}
