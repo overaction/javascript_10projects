405aa4e8f5fb46a9136df4cfa75fb2392e61bdb8
overaction <52850392+overaction@users.noreply.github.com>
2020-10-17 23:05:28 +0900

    9. password generator - add options
    checkbox : will you contain uppercase, lowercase, number, symbol

--- 9. password generate/main.js
+++ 9. password generate/main.js
@@ -10,18 +10,73 @@ const symbol = document.querySelector('.symbol');
 const createBtn = document.querySelector('.create');
 
 let result = ``;
-let characters = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz`;
+let charlength;
+let characterU = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
+let characterL = `abcdefghijklmnopqrstuvwxyz`;
+let symbols = "!@#$%^&*()_+=";
 let numbers = `0123456789`;
 
-function generatePW(char, length, up, low, num) {
-    const charlength = char.length;
-    createBtn.addEventListener('click',(e) => {
-        for ( let i = 0; i < length; i++ ) {
-            result += char.charAt(Math.floor(Math.random() * charlength));
-        }
-        password.innerText = result;
-        result = ``;
-    })
+function getUppercase() {
+    return characterU[Math.floor(Math.random() * characterU.length)];
 }
 
-generatePW(characters,length.value);
+function getLowercase() {
+    return characterL[Math.floor(Math.random() * characterL.length)];
+}
+
+function getNumber() {
+    return numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+function getSymbol() {
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+function generatePW() {
+    console.log(length.value);
+    if (uppercase.checked) {
+        result += getUppercase();
+    }
+
+    if (lowercase.checked) {
+        result += getLowercase();
+    }
+
+    if (number.checked) {
+        result += getNumber();
+    }
+
+    if (symbol.checked) {
+        result += getSymbol();
+    }
+    for(let i=result.length; i<length.value; i++) {
+        const ext = extraPW();
+        result += ext;
+    }
+    password.innerText = result;
+    result = ``;
+}
+
+function extraPW() {
+    const expw = [];
+    if (uppercase.checked) {
+        expw.push(getUppercase());
+    }
+
+    if (lowercase.checked) {
+        expw.push(getLowercase());
+    }
+
+    if (number.checked) {
+        expw.push(getNumber());
+    }
+
+    if (symbol.checked) {
+        expw.push(getSymbol());
+    }
+    if(expw.length === 0) return '';
+
+    return expw[Math.floor(Math.random()*expw.length)];
+}
+
+createBtn.addEventListener('click',generatePW);
