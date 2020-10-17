4f82beea3f110b1e34f80e3e83ff8bfa803b73c1
overaction <52850392+overaction@users.noreply.github.com>
2020-10-17 15:14:33 +0900

    8. draw app - controls paint size

---
+++ 8. drawingapp/main.js
@@ -0,0 +1,45 @@
+'use strict';
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const size = document.getElementById('size');
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const input = document.getElementById('color');
+const clearBtn = document.getElementById('clear');
+
+console.log(size.innerText);
+
+let x = 50;
+let y = 50;
+let click = false;
+
+canvas.addEventListener('mousedown',(e) => {
+    click = true;
+})
+
+canvas.addEventListener('mouseup', (e) => {
+    click = false;
+})
+
+canvas.addEventListener('mousemove', (e) => {
+    const x = e.offsetX;
+    const y = e.offsetY;
+    if(click)
+        drawCircle(x,y);
+});
+
+increaseBtn.addEventListener('click', () => {
+    size.innerText = Number(size.innerText)+5;
+});
+
+
+decreaseBtn.addEventListener('click', () => {
+    size.innerText = Number(size.innerText)-5;
+})
+
+function drawCircle(x,y) {
+    ctx.beginPath();
+    ctx.arc(x,y,size.innerText,0,Math.PI*2);
+    ctx.fill();
+}
+
